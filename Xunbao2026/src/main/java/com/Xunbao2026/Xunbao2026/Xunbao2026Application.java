36466b859b1244924de1f5b78c10c4fdae0a6cf1
Just-a-guy-o7 <pandey1divyanshu@gmail.com>
2026-03-31 17:55:22 +0530

    Initialize Xunbao2026 project with Maven setup and basic Spring Boot application structure

---
+++ Xunbao2026/src/main/java/com/Xunbao2026/Xunbao2026/Xunbao2026Application.java
@@ -0,0 +1,13 @@
+package com.Xunbao2026.Xunbao2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xunbao2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Xunbao2026Application.class, args);
+	}
+
+}
---
+++ Xunbao2026/src/test/java/com/Xunbao2026/Xunbao2026/Xunbao2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Xunbao2026.Xunbao2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Xunbao2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
